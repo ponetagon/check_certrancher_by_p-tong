e9de5cec1f440a2741e8dc007c23750477af7cb3
Ponetagon <ponetagon@test.com>
2022-10-27 15:52:58 +0700

    first commit

---
+++ cert.sh
@@ -0,0 +1,4 @@
+cert=$(docker exec -it rancher kubectl get secret k3s-serving -n kube-system  -o yaml | sed -n 3p | awk '{print $2}')
+echo $cert |base64 -di >> cert.crt 
+openssl x509 -enddate -noout -in cert.crt
+rm cert.crt
