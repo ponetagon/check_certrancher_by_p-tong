11ed55ae89b0d0437c381fc70fe41c2b35279a29
Ponetagon <ponetagon@test.com>
2022-11-11 13:59:57 +0700

    first commit

--- x590.sh
+++ x590.sh
@@ -1,4 +1,4 @@
-docker exec -it rancher sh -c "rm -rf /var/lib/rancher/k3s/server/tls/dynamic-cert.json"
-docker exec -it rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n kube-system k3s-serving"
-docker exec -it rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n cattle-system serving-cert"
+docker exec  rancher sh -c "rm -rf /var/lib/rancher/k3s/server/tls/dynamic-cert.json"
+docker exec  rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n kube-system k3s-serving"
+docker exec  rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n cattle-system serving-cert"
 docker restart rancher
