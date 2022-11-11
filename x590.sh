4f1dd20fdddb14909ce01c67073669ad250a7139
Ponetagon <ponetagon@test.com>
2022-11-11 14:02:28 +0700

    edit x509

--- x590.sh
+++ x590.sh
@@ -1,4 +1,4 @@
-docker exec  rancher sh -c "rm -rf /var/lib/rancher/k3s/server/tls/dynamic-cert.json"
-docker exec  rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n kube-system k3s-serving"
-docker exec  rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n cattle-system serving-cert"
+docker exec -i rancher sh -c "rm -rf /var/lib/rancher/k3s/server/tls/dynamic-cert.json"
+docker exec -i rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n kube-system k3s-serving"
+docker exec -i rancher sh -c "k3s kubectl --insecure-skip-tls-verify  delete secrets -n cattle-system serving-cert"
 docker restart rancher
